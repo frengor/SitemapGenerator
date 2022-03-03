7f06438a48978957fd3cafeb27c8fbc8c66001b5
fren_gor <goro@frengor.com>
2022-03-04 00:23:52 +0100

    Add cli

---
+++ src/input.rs
@@ -0,0 +1,82 @@
+use std::collections::HashSet;
+
+use clap::{CommandFactory, ErrorKind, Parser};
+use url::{Position, Url};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Input {
+    domains_to_analyze: Vec<String>,
+    #[clap(short, long)]
+    starting_points: Option<Vec<String>>,
+    #[clap(short, long)]
+    additional_links: Option<Vec<String>>,
+}
+
+pub(super) struct Options {
+    pub domains_to_analyze: HashSet<Url>,
+    pub starting_points: HashSet<Url>,
+    pub additional_links: Option<HashSet<Url>>,
+}
+
+impl Options {
+    #[inline]
+    pub(super) fn from_cli() -> Options {
+        Input::parse().into()
+    }
+}
+
+impl From<Input> for Options {
+    fn from(input: Input) -> Self {
+        if input.domains_to_analyze.is_empty() {
+            error("No domain has been provided.".to_string());
+        }
+        let mut opts = Options {
+            domains_to_analyze: input.domains_to_analyze.iter().map(|str| domain_validator(str)).collect(),
+            starting_points: input.starting_points.map_or(HashSet::new(), |vec| vec.iter().map(|str| url_validator(str)).collect()),
+            additional_links: input.additional_links.map(|vec| vec.iter().map(|str| url_parser(str)).collect()),
+        };
+        opts.domains_to_analyze.iter().for_each(|url| { opts.starting_points.insert(url.clone()); });
+        opts
+    }
+}
+
+#[inline]
+fn error(error: String) -> ! {
+    Input::command().error(
+        ErrorKind::InvalidValue,
+        error,
+    ).exit()
+}
+
+fn url_parser(url: &str) -> Url {
+    match Url::parse(url) {
+        Ok(url) => url,
+        Err(parse_err) => error(parse_err.to_string()),
+    }
+}
+
+fn url_validator(url: &str) -> Url {
+    let url = url_parser(url);
+    if url.cannot_be_a_base() {
+        error(format!(r#""{url}" is not an abstract URL"#))
+    }
+    url
+}
+
+fn domain_validator(domain: &str) -> Url {
+    let domain = url_validator(domain);
+    {
+        let sub_url = &domain[Position::BeforePath..];
+        if !(sub_url.is_empty() || sub_url == "/") {
+            error(format!(r#""{domain}" is not a valid domain"#))
+        }
+    }
+    domain
+}
+
+#[test]
+fn verify_app() {
+    use clap::CommandFactory;
+    Input::command().debug_assert();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #![allow(non_snake_case)]
 
+use crate::input::Options;
+
 mod utils;
 mod site_analyzer;
+mod input;
 
 const CONCURRENT_TASKS: usize = 64;
 
 fn main() {
+    let options = Options::from_cli();
+
     let sites_to_analyze = vec![
         "185.25.204.194",
         "https://docs.rs/hyper/0.14.16/hyper/client/struct.Client.html",
         "https://frengor.com/",
         "https://frengor.com/UltimateAdvancementAPI/",
     ];
+
+    let sites_to_analyze = options.starting_points;
 
     let sites_to_analyze = sites_to_analyze.into_iter().map(|site| site.to_string()).collect();
 
@@ -25,6 +32,10 @@ fn main() {
 
     for site in sites {
         println!("{}", site.as_ref());
+    }
+
+    if let Some(additional_links) = options.additional_links {
+        additional_links.iter().map(|site| site.to_string()).for_each(|site| println!("{}", site));
     }
 }
 
